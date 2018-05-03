d5330cf8b82fd3426e110d115b6b05419b2078c1
Hein Bekker <hein@netbek.co.za>
2018-05-03 04:04:25 +0200

    Add Finch

--- vega/src/js/bundle.js
+++ vega/src/js/bundle.js
@@ -1,5 +1,4 @@
+import finch from '@netbek/finch';
 import regression from 'regression';
-import toVega from 'to-vega';
-import vegaEmbed from 'vega-embed';
 
-export {regression, toVega, vegaEmbed};
+export {finch, regression};
--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,10 @@ module.exports = {
       }
     ]
   },
+  externals: {
+    vega: 'vega',
+    'vega-embed': 'vegaEmbed'
+  },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
